056f47efe105ee9b30c104cc321ccf7faa08d1b2
iscoutto <isacoutosoares@gmail.com>
2024-09-22 20:16:48 -0300

    update api role

--- infra/modules/api_gateway/api_gateway.tf
+++ infra/modules/api_gateway/api_gateway.tf
@@ -21,6 +21,7 @@ resource "aws_api_gateway_integration" "login_integration" {
   rest_api_id             = aws_api_gateway_rest_api.nutrifai_api.id
   resource_id             = aws_api_gateway_resource.login.id
   http_method             = aws_api_gateway_method.login_method.http_method
+  credentials             = aws_iam_role.execution_role.arn
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:sa-east-1:${data.aws_caller_identity.current.account_id}:function:lambda-login/invocations"
@@ -44,6 +45,7 @@ resource "aws_api_gateway_integration" "register_integration" {
   rest_api_id             = aws_api_gateway_rest_api.nutrifai_api.id
   resource_id             = aws_api_gateway_resource.register.id
   http_method             = aws_api_gateway_method.register_method.http_method
+  credentials             = aws_iam_role.execution_role.arn
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:sa-east-1:${data.aws_caller_identity.current.account_id}:function:lambda-login/invocations"
@@ -67,6 +69,7 @@ resource "aws_api_gateway_integration" "logout_integration" {
   rest_api_id             = aws_api_gateway_rest_api.nutrifai_api.id
   resource_id             = aws_api_gateway_resource.logout.id
   http_method             = aws_api_gateway_method.logout_method.http_method
+  credentials             = aws_iam_role.execution_role.arn
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:sa-east-1:${data.aws_caller_identity.current.account_id}:function:lambda-login/invocations"
