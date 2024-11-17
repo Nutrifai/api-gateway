49e728dfd6909f6d739d59b562b4213ec95c1e8b
iscoutto <isacoutosoares@gmail.com>
2024-11-17 16:47:44 -0300

    Add new book path

--- infra/modules/api_gateway/api_gateway.tf
+++ infra/modules/api_gateway/api_gateway.tf
@@ -219,6 +219,30 @@ resource "aws_api_gateway_integration" "update_appointments_integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:sa-east-1:${data.aws_caller_identity.current.account_id}:function:lambda-nutritionist/invocations"
 }
 
+# PUT /nutritionists/<nutriId>/appointments/<appointmentId>/book
+resource "aws_api_gateway_resource" "nutri_appointments_book" {
+  rest_api_id = aws_api_gateway_rest_api.nutrifai_api.id
+  parent_id   = aws_api_gateway_resource.nutri_appointments_id.id
+  path_part   = "book"
+}
+
+resource "aws_api_gateway_method" "nutri_appointments_book_method" {
+  rest_api_id   = aws_api_gateway_rest_api.nutrifai_api.id
+  resource_id   = aws_api_gateway_resource.nutri_appointments_book.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "nutri_appointments_book_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.nutrifai_api.id
+  resource_id             = aws_api_gateway_resource.nutri_appointments_book.id
+  http_method             = aws_api_gateway_method.nutri_appointments_book_method.http_method
+  credentials             = aws_iam_role.execution_role.arn
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:sa-east-1:${data.aws_caller_identity.current.account_id}:function:lambda-nutritionist/invocations"
+}
+
 # AUTHORIZER
 resource "aws_api_gateway_authorizer" "session_authorizer" {
   name                   = "session-authorizer"
@@ -273,6 +297,9 @@ resource "aws_api_gateway_deployment" "deployment" {
       aws_api_gateway_resource.nutri_appointments_id.id,
       aws_api_gateway_method.update_nutri_appointments_method.id,
       aws_api_gateway_integration.update_appointments_integration.id,
+      aws_api_gateway_resource.nutri_appointments_book.id,
+      aws_api_gateway_method.nutri_appointments_book_method.id,
+      aws_api_gateway_integration.nutri_appointments_book_integration.id,
     ]))
   }
 
